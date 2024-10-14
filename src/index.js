73f0860e8d6a95da30139abbdc9b030fae8d84cf
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-14 14:02:49 +0530

    added api.js to make HTTP request using AXIOS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// for debugging purposes
+// import searchImages from "./api";
+// searchImages();
 
 const rootEl = document.getElementById("root");
 
